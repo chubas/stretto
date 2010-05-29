ec34d82918c3393ef1749e9ab100d7364a9a0dba
Rubén Medellín <ruben.medellin.c@gmail.com>
2010-05-29 16:04:08 -0500

    Added spec for ties

--- spec/parser/rests_spec.rb
+++ spec/parser/rests_spec.rb
@@ -12,13 +12,29 @@ describe "parsing rests" do
     end
   end
 
-  context "allowing a rest to have any modifier a single note can have" do
-    it "should accept accidentals" do
-      Stretto::Parser.new("Rb Rbb R# R## Rn").should be_valid
+  context "parsing rest modifiers" do
+    it "should allow duration modifier" do
+      Stretto::Parser.new("Rw").should be_valid
+      Stretto::Parser.new("R/3.0").should be_valid
+      Stretto::Parser.new("Ri*3:4").should be_valid
     end
 
-    it "should allow octaves" do
-      Stretto::Parser.new("R0 R6 R10").should be_valid
+    it "should not accept accidentals" do
+      Stretto::Parser.new("R#").should_not be_valid
+      Stretto::Parser.new("Rb").should_not be_valid
+      Stretto::Parser.new("R##").should_not be_valid
+      Stretto::Parser.new("Rbb").should_not be_valid
+      Stretto::Parser.new("Rn").should_not be_valid
+    end
+
+    it "should not allow octaves" do
+      Stretto::Parser.new("R7").should_not be_valid
+    end
+
+    it "should not accept attack nor decay velocities" do
+      Stretto::Parser.new("Ra100").should_not be_valid
+      Stretto::Parser.new("Rd100").should_not be_valid
+      Stretto::Parser.new("Ra100d100").should_not be_valid
     end
   end
 

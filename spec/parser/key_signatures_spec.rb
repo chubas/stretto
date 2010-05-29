7d5b2729f1f7f092e3b2a4b0286d64a971119168
Rubén Medellín <ruben.medellin.c@gmail.com>
2010-05-29 16:49:01 -0500

    Key signature spec

---
+++ spec/parser/key_signatures_spec.rb
@@ -0,0 +1,22 @@
+require File.join(File.dirname(__FILE__), '../spec_helper')
+
+describe "parsing key signatures" do
+
+  it "should parse key signatures" do
+    Stretto::Parser.new("KCmaj").should be_valid
+    Stretto::Parser.new("KCbmaj").should be_valid
+    Stretto::Parser.new("KCmin").should be_valid
+    Stretto::Parser.new("KCbmin").should be_valid
+  end
+
+  it "should always require note, followed by maj or min in a key signature" do
+    Stretto::Parser.new("KC").should_not be_valid
+    Stretto::Parser.new("Kmaj").should_not be_valid
+  end
+
+  it "should accept only key and accidental with time signatures" do
+    Stretto::Parser.new("KC3min").should_not be_valid
+    Stretto::Parser.new("KCwmaj").should_not be_valid
+  end
+
+end

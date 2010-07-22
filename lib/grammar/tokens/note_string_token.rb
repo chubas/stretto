79a15c2cf42886f52799010192d1b49c0267bbf5
Rubén Medellín <ruben.medellin.c@gmail.com>
2010-07-22 00:53:13 -0500

    Refactored note_string token, joined modules

--- lib/grammar/tokens/note_string_token.rb
+++ lib/grammar/tokens/note_string_token.rb
@@ -1,9 +1,56 @@
 module Stretto
   module Tokens
 
-    # TODO: Remove from Tokens Section, or create a new file now that NoteStringToken has disappeared
+
+    module NotePitchToken
+
+      def accidental
+        nil
+      end
+
+      def octave
+        nil
+      end
+
+      def key
+        nil
+      end
+
+      def pitch
+        klass = if _pitch.is_numeric?
+          Stretto::Value::NumericValue
+        else
+          Stretto::Value::VariableValue
+        end
+        Stretto::Value.new(klass.new(_pitch.text_value))
+      end
+    end
+
+    #------------------------------------------------------------------
+
+    module NoteKeyAccidentalOctaveToken
+
+      def key
+        note_key.key.text_value
+      end
+
+      def accidental
+        note_key.accidental.text_value if note_key.accidental and note_key.accidental.text_value.present?
+      end
+
+      def pitch
+        nil
+      end
+
+      def octave
+        _octave.text_value if _octave and _octave.text_value.present?
+      end
+    end
+
+    #------------------------------------------------------------------
 
     module WithNoteStringToken
+
       def octave
         note_string.octave
       end

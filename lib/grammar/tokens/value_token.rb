1847b736cf7dfa0b5da072f9b4206d99e3c3a240
Rubén Medellín <ruben.medellin.c@gmail.com>
2010-07-22 01:15:19 -0500

    Joined numeric and variable tokens into one file

--- lib/grammar/tokens/value_token.rb
+++ lib/grammar/tokens/value_token.rb
@@ -2,11 +2,15 @@ module Stretto
   module Tokens
 
     module NumericToken
-
       def wrap
         Stretto::Value::NumericValue.new(text_value.to_f)
       end
+    end
 
+    module VariableToken
+      def wrap
+        Stretto::Value::VariableValue.new(name.text_value)
+      end
     end
 
   end

a11826e5bf807213e2616489c5aa57ae1c708698
Rubén Medellín <chubas7@gmail.com>
2010-11-22 18:01:24 -0600

    Added Gemfile to control required gems

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'http://gemcutter.org'
+
+gem 'treetop'
+gem 'midiator'
+gem 'midilib'
+
+gem 'rspec', '1.3.0'
--- lib/stretto.rb
+++ lib/stretto.rb
@@ -1,3 +1,6 @@
+require 'rubygems'
+require 'bundler/setup'
+
 require 'treetop'
 require 'polyglot'
 require 'midiator'
